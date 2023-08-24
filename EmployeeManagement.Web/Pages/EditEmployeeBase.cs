7183c1a31096e8e6fe77fdb8bffdc50e7557a6ca
karthi123456 <karthipkm1993@gmail.com>
2023-08-24 09:41:38 +0530

    Custom Input select create and seperate Razor Library. Custom Input select main reason selected value does not support integer value only for string.

--- EmployeeManagement.Web/Pages/EditEmployeeBase.cs
+++ EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -18,15 +18,12 @@ namespace EmployeeManagement.Web.Pages
         public IDepartmentService DepartmentService { get; set; }
         public List<Department> Departments { get; set; } = new List<Department>();
 
-        public string DepartmentId { get; set; }
-
         [Parameter]
         public string Id { get; set; }
         protected async override Task OnInitializedAsync()
         {
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
             Departments = (await DepartmentService.GetDepartments()).ToList();
-            DepartmentId = Employee.DepartmentId.ToString();
         }
     }
 }
